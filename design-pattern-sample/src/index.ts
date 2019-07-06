0efc4723acdb84813dfe40c88c3f72f1c90c5471
Varis Darasirikul <varis.dara@gmail.com>
2019-07-06 14:09:05 +0800

    Finish google map sample

--- design-pattern-sample/src/index.ts
+++ design-pattern-sample/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import {} from '@types/googlemaps';
+
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const user = new User();
+// console.log(user);
+
+const company = new Company();
+// console.log(company);
+
+const customMap = new CustomMap('map');
+customMap.addMarker(user);
+customMap.addMarker(company);
