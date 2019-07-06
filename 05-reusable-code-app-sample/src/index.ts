3a9b337019993b06c539e1cc5649875d5984cc3a
Varis Darasirikul <varis.dara@gmail.com>
2019-07-07 03:31:39 +0800

    Create analysis class

--- 05-reusable-code-app-sample/src/index.ts
+++ 05-reusable-code-app-sample/src/index.ts
@@ -1,6 +1,8 @@
-import { MatchResult } from './MatchResult';
 import { MatchReader } from './interfaces-method/MatchReader';
 import { CsvFileReader } from './interfaces-method/CsvFileReader';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
+import { ConsoleReport } from './ConsoleReport';
 
 // const reader = new MatchReader('data/football.csv');
 // reader.read();
@@ -9,21 +11,9 @@ const csvFileReader = new CsvFileReader('data/football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-// const printMatchResult = (match: string[]): MatchResult => {
-//   if (match[5] === 'H') {
-//     return MatchResult.HomeWin;
-//   }
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
 
-//   return MatchResult.AwayWin;
-// };
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`Man United won: ${manUnitedWins}`);
+summary.buildAndPrintReport(matchReader.matches);
